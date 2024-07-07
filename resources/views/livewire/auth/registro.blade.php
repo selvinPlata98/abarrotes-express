c5d38e338bb0f543d8c9b67f679e450e0b0ef852
SelvinPlata98 <selvin.plata@unah.hn>
2024-07-07 11:22:39 -0500

    🐧Modificación de la interfaz y creación de plantilla

--- resources/views/livewire/auth/registro.blade.php
+++ resources/views/livewire/auth/registro.blade.php
@@ -11,14 +11,14 @@
                             <input wire:model="name" type="name" id="register-email" class="w-full px-3 py-1 border focus:border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-primary" required>
                         </div>
                         <div class="mb-3">
-                            <label for="register-email" class="block ">Email</label>
+                            <label for="register-email" class="block ">correo</label>
                             <input wire:model="email" type="email" id="register-email" class="w-full px-3 py-1 border focus:border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-primary" required>
                         </div>
                         <div class="mb-3">
-                            <label for="register-password" class="block ">Password</label>
+                            <label for="register-password" class="block ">Contraseña</label>
                             <input wire:model="password" type="password" id="register-password" class="w-full px-3 py-1 border focus:border-transparent rounded-full focus:outline-none focus:ring-2 focus:ring-primary" required>
                         </div>
-                        <button type="submit" class="bg-primary text-red border border-primary hover:bg-transparent hover:text-primary py-2 px-3 rounded-full w-full">Register</button>
+                        <button type="submit" class="bg-primary text-red border border-primary hover:bg-transparent hover:text-primary py-2 px-3 rounded-full w-full">Registra</button>
                     </form>
           </div>
   </div>      
